063f18c4f086a2ef5ab313a2e31cdf5f8a110c86
santoshpavan <santoshpavan.psp@gmail.com>
2021-02-08 01:06:59 -0500

    POST, PUT and DELETE actions for Customer

--- src/main/java/com/springframework/brewery/services/CustomerService.java
+++ src/main/java/com/springframework/brewery/services/CustomerService.java
@@ -2,6 +2,14 @@ package com.springframework.brewery.services;
 
 import com.springframework.brewery.web.model.CustomerDto;
 
+import java.util.UUID;
+
 public interface CustomerService {
     CustomerDto getCustomerById();
+
+    CustomerDto saveNewCustomer(CustomerDto customerDto);
+
+    void updateCustomer(UUID customerId);
+
+    void deleteCustomer(UUID customerId);
 }
--- src/main/java/com/springframework/brewery/web/controller/CustomerController.java
+++ src/main/java/com/springframework/brewery/web/controller/CustomerController.java
@@ -2,12 +2,10 @@ package com.springframework.brewery.web.controller;
 
 import com.springframework.brewery.services.CustomerService;
 import com.springframework.brewery.web.model.CustomerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -23,5 +21,28 @@ public class CustomerController {
     @GetMapping("/{customerId}")
     public ResponseEntity<CustomerDto> getCustomer(@PathVariable("customerId")UUID customerId) {
         return new ResponseEntity<>(customerService.getCustomerById(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody CustomerDto customerDto) {
+        CustomerDto savedDto = customerService.saveNewCustomer(customerDto);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        //TODO: add hostname to the URL below
+        httpHeaders.add("Location", "/api/v1/customer" + savedDto.getId().toString());
+
+        return new ResponseEntity(httpHeaders, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{customerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void handlePut(@PathVariable("customerId") UUID customerId) {
+        customerService.updateCustomer(customerId);
+    }
+
+    @DeleteMapping("/{customerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void handleDelete(@PathVariable("customerId") UUID customerId) {
+        customerService.deleteCustomer(customerId);
     }
 }
