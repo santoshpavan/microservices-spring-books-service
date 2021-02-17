1f7f14278445ee7bc0918175a3fc4bed370d61ac
santoshpavan <santoshpavan.psp@gmail.com>
2021-02-16 23:34:05 -0500

    Bean Validations; closes #8

--- src/main/java/com/springframework/msscbookservice/web/model/BookDto.java
+++ src/main/java/com/springframework/msscbookservice/web/model/BookDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -14,14 +18,26 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder //Builder Pattern
 public class BookDto {
+    // validation to NULL so that it cannot be set externally
+    @Null
     private UUID id;
+    @Null
     private Integer version;
     // Use OffsetDateTime to make times based on the UTC date
+    @Null
     private OffsetDateTime createdDate;
+    @Null
     private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
     private String bookName;
+    @NotNull
     private BookGenreEnum bookGenre;
+    @Positive
+    @NotNull
     private long upc;
+    @Positive
+    @NotNull
     private BigDecimal price;
     private Integer quantityOnHand;
 }
